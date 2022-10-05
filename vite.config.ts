d86b1e3aa0d091ca5cd6dd7a53803aa13e5b0d37
Borris <borris.wiria@gmail.com>
2022-10-05 16:37:56 +0400

    push netlify

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,18 @@ import { defineConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import netlifyEdge from "@netlify/vite-plugin-netlify-edge";
 
 export default defineConfig(() => {
   return {
     ssr: { target: "webworker", noExternal: true },
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    plugins: [
+      qwikCity(),
+      qwikVite({
+        ssr: { outDir: "netlify/edge-functions/entry.netlify-edge" },
+      }),
+      tsconfigPaths(),
+      netlifyEdge({ functionName: "entry.netlify-edge" }),
+    ],
   };
 });
